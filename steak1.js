c9c21380a928c10253ef625a999aa7833533c7ac
JuliusVermeeren <122489348+JuliusVermeeren@users.noreply.github.com>
2023-01-23 12:59:31 +0100

    Add files via upload

---
+++ steak1.js
@@ -0,0 +1,27 @@
+function Switch1() {
+  let p1f = document.getElementById("prod1f");
+  let p1b = document.getElementById("prod1b");
+  let p1bm = document.getElementById("prod1bM");
+  let p1fm = document.getElementById("prod1fM");
+  
+  if (p1f.style.display = "block") {
+    p1f.style.display = "none";
+    p1b.style.display = "block";
+    p1bm.style.display = "none";
+    p1fm.style.display = "block";
+  }
+  }
+  
+  function Switch1r() {
+  let p1f = document.getElementById("prod1f");
+  let p1b = document.getElementById("prod1b");
+  let p1bm = document.getElementById("prod1bM");
+  let p1fm = document.getElementById("prod1fM");
+
+  if (p1f.style.display = "none") {
+    p1f.style.display = "block";
+    p1b.style.display = "none";
+    p1bm.style.display = "block";
+    p1fm.style.display = "none";
+  }
+  }
